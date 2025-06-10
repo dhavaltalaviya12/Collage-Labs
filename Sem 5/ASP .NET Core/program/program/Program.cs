97ea04769e8fdcb6944a8e24405e9801d5bf0f2e
Dhaval Talaviya <146553819+dhavaltalaviya12@users.noreply.github.com>
2025-06-10 09:29:28 +0530

    Add files via upload

---
+++ Sem 5/ASP .NET Core/program/program/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter Name : ");
+            //String name = Console.ReadLine();
+            //Console.WriteLine("My name is " + name);
+            //Console.Write("Enter Address : ");
+            //String address = Console.ReadLine();
+            //Console.WriteLine("My Address is " + address);
+            //Console.Write("Enter City : ");
+            //String city = Console.ReadLine();
+            //Console.WriteLine("My City is " + city);
+            //Console.Write("Enter Number : ");
+            //long number = long.Parse(Console.ReadLine());
+            //Console.WriteLine("My NUmber is " + number);
+
+            Class1 obj = new Class1();
+            Console.WriteLine(obj.add(3,4));
+        }
+    }
+}
