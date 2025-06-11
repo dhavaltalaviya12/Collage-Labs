5513f1aa0429e414c474129006eed268917cc5ed
Dhaval Talaviya <146553819+dhavaltalaviya12@users.noreply.github.com>
2025-06-11 13:39:12 +0530

    Add files via upload

---
+++ Sem 5/ASP .NET Core/program/Lab 2/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ASP_.NET_Labs;
+using ASP_.NET_Labs.Lab_2;
+
+
+//Pr 1
+//Candidates cd = new Candidates();
+//cd.GetCandidateDetails();
+//cd.DisplayCandidateDetails();
+//----------------------------------------------------------------------------------------------------------
+
+//Pr 2
+//Staff[] staff = new Staff[5];
+//for (int i = 0; i < 5; i++)
+//{
+//    staff[i] = new Staff();
+//    staff[i].GetStaffDetails();
+//    Console.WriteLine("--------------------------------------");
+//}
+
+//Console.WriteLine("Department of HOD");
+
+//for (int i = 0; i < 5; i++)
+//{
+//    if(staff[i].Department == "HOD")
+//    {
+//        Console.WriteLine(staff[i].Name);
+//        Console.WriteLine(staff[i].Salary);
+
+//    }
+//}
+//-------------------------------------------------------------------------------------------------------------
+
+//Pr 3
+BankAccount bankAccount=new BankAccount();
+bankAccount.GetAccountDetails();
+bankAccount.DisplayAccountDetails();
