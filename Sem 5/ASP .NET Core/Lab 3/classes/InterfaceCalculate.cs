019949272b2b3e1707a29f2136f407bdc901635e
Dhaval Talaviya <146553819+dhavaltalaviya12@users.noreply.github.com>
2025-06-18 19:49:41 +0530

    Add files via upload

---
+++ Sem 5/ASP .NET Core/Lab 3/classes/InterfaceCalculate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_.NET_Lab.classes
+{
+    internal class InterfaceCalculate
+    {
+        public interface Calculate
+        {
+            public int Addition(int a, int b);
+            public int Subtraction(int a, int b);
+        }
+
+        public class Result : Calculate
+        {
+            public int Addition(int a, int b)
+            {
+                return a + b;
+            }
+
+            public int Subtraction(int a, int b)
+            {
+                return a - b;
+            }
+        }
+    }
+}
